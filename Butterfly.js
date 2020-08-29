9de2d10fa355cfddea364f86309d10f87df58f0e
Adam Nizol <32315521+AdamNizol@users.noreply.github.com>
2020-08-29 16:23:05 -0700

    shrink particle size throughout drawing

--- Butterfly.js
+++ Butterfly.js
@@ -8,6 +8,7 @@ var  VerletPhysics2D = toxi.physics2d.VerletPhysics2D,
      Rect = toxi.geom.Rect,
      Vec2D = toxi.geom.Vec2D;
 
+let strokeControl;
 let p1, physics, bg, curImage;
 let ready = false;
 let particles = [];
@@ -16,27 +17,25 @@ let particlesRight = [];
 let particlesBottom = [];
 let frameCounter = 0;
 let imageNum = 1;
+let frameCount = 12;
+let minParticleSize = 5;
+let maxParticleSize = 15;
 
-//let curImage = [];
+let particleSize = maxParticleSize;
+
 let butterflyUrl = "Images/butterfly.png"
 getImg(butterflyUrl,960,660);
 
 function setup() {
-  //createCanvas(window.innerWidth,window.innerHeight);
+  strokeControl = document.getElementById("strokeEnabledBox");
+
   createCanvas(1280, 720);
   frameRate(60);
   physics = new VerletPhysics2D();
   physics.setWorldBounds(new Rect(0, 0, width, height));
 
-  /*window.addEventListener('resize', function(){
-    resizeCanvas(window.innerWidth,window.innerHeight);
-    physics.setWorldBounds(new Rect(-width/2, -height/2, width, height));
-    physicsLeft.setWorldBounds(new Rect(-width/2, -height/2, width, height));
-  } );*/
-
   bg = loadImage(butterflyUrl);
   physics.addBehavior(new GravityBehavior(new Vec2D(0,0.1)));
-  //physics.addBehavior(new GravityBehavior(new Vec2D(0,-0.05)));
   p1 = new VerletParticle2D(10,10);
   physics.addParticle(p1);
   physics.addBehavior(new AttractionBehavior(p1, 350, 0.1));
@@ -44,7 +43,6 @@ function setup() {
   for(let i =0; i<600; i++){
     particles.push(new VerletParticle2D(Math.floor(Math.random()*width),Math.random()*660));
     physics.addParticle(particles[i]);
-    //physics.addBehavior(new AttractionBehavior(particles[i], Math.random()*50+ 225, Math.random()*0.5-0.5));
   }
 
   physicsLeft = new VerletPhysics2D();
@@ -56,7 +54,6 @@ function setup() {
   for(let i =0; i<600; i++){
     particlesLeft.push(new VerletParticle2D(Math.floor(Math.random()*width),Math.random()*660));
     physicsLeft.addParticle(particlesLeft[i]);
-    //physics.addBehavior(new AttractionBehavior(particles[i], Math.random()*50+ 225, Math.random()*0.5-0.5));
   }
 
   physicsRight = new VerletPhysics2D();
@@ -68,19 +65,16 @@ function setup() {
   for(let i =0; i<600; i++){
     particlesRight.push(new VerletParticle2D(Math.floor(Math.random()*width),Math.random()*660));
     physicsRight.addParticle(particlesRight[i]);
-    //physics.addBehavior(new AttractionBehavior(particles[i], Math.random()*50+ 225, Math.random()*0.5-0.5));
   }
 
   physicsBottom = new VerletPhysics2D();
   physicsBottom.setWorldBounds(new Rect(0, 0, width, height));
   physicsBottom.addBehavior(new GravityBehavior(new Vec2D(0,-0.1)));
   physicsBottom.addParticle(p1);
-  //physicsBottom.addBehavior(new AttractionBehavior(p1, 350, 0.1));
 
   for(let i =0; i<600; i++){
     particlesBottom.push(new VerletParticle2D(Math.floor(Math.random()*width),Math.random()*660));
     physicsBottom.addParticle(particlesBottom[i]);
-    //physics.addBehavior(new AttractionBehavior(particles[i], Math.random()*50+ 225, Math.random()*0.5-0.5));
   }
 
 
@@ -93,84 +87,76 @@ function draw() {
     physicsLeft.update();
     physicsRight.update();
     physicsBottom.update();
-    if(frameCounter >= 40){
+    let framesPerImage = 70;
+    particleSize = maxParticleSize - ((maxParticleSize - minParticleSize)*(frameCounter/framesPerImage));
+    if(frameCounter >= framesPerImage){
       frameCounter = 0;
-      getImg("Images/img"+("000"+imageNum).slice(-4)+".jpg", 1280, 720);
+      getImg("Images/img"+("000"+imageNum).slice(-4)+".jpg", 550, 640);
       imageNum++;
+      imageNum = Math.max(imageNum%(frameCount+1), 1);
     }
 
     background(250, 250, 250,0);
-    //fill(100,100,100, 30);
-    //rect(0,0,width,height);
     fill(127);
-    p1.x = mouseX;
-    p1.y = mouseY;
-    let col = getPixel(p1.x,p1.y);
+    // p1.x = mouseX;
+    // p1.y = mouseY;
+    // let col = getPixel(p1.x,p1.y);
+    //
+    // fill(col[0],col[1],col[2],col[3]);
+    // ellipse(p1.x, p1.y, 20,20);
 
-    fill(col[0],col[1],col[2],col[3]);
-    ellipse(p1.x, p1.y, 20,20);
-
-    stroke(0,0,0,0);
+    stroke('rgba(0,0,0,0.05)');
+    if(strokeControl.checked){
+      strokeWeight(1)
+    }else{
+      strokeWeight(0)
+    }
     for(let i=0; i<particles.length;i++){
       if(particles[i].y == height){
         physics.removeParticle(particles[i]);
-        //particles[i] =  new VerletParticle2D(Math.floor(Math.random()*width),Math.floor(Math.random()*50));
         particles[i] =  new VerletParticle2D(Math.floor(Math.random()*width),Math.floor(Math.random()*height));
         physics.addParticle(particles[i]);
-        /*
-        particles[i].y = 0;
-        particles[i].x = Math.floor(Math.random()*959);
-        particles[i].clearVelocity();
-        particles[i].clearForce();
-        console.log(particles[i].getForce())*/
       }
 
       col = getPixel(particles[i].x,particles[i].y);
       fill(col[0],col[1],col[2],125);
-      //rect(particles[i].x-5, particles[i].y-5,10,10);
-      ellipse(particles[i].x, particles[i].y, 7,7);
+      ellipse(particles[i].x, particles[i].y, particleSize,particleSize);
     }
 
     for(let i=0; i<particlesLeft.length;i++){
       if(particlesLeft[i].x == width){
         physicsLeft.removeParticle(particlesLeft[i]);
-        //particlesLeft[i] =  new VerletParticle2D(Math.floor(Math.random()*50),Math.floor(Math.random()*height));
         particlesLeft[i] =  new VerletParticle2D(Math.floor(Math.random()*width),Math.floor(Math.random()*height));
         physicsLeft.addParticle(particlesLeft[i]);
       }
 
       col = getPixel(particlesLeft[i].x,particlesLeft[i].y);
       fill(col[0],col[1],col[2],125);
-      //rect(particles[i].x-5, particles[i].y-5,10,10);
-      ellipse(particlesLeft[i].x, particlesLeft[i].y, 7,7);
+      ellipse(particlesLeft[i].x, particlesLeft[i].y, particleSize,particleSize);
     }
 
     for(let i=0; i<particlesRight.length;i++){
       if(particlesRight[i].x == 0){
         physicsRight.removeParticle(particlesRight[i]);
-        //particlesRight[i] =  new VerletParticle2D(width - Math.floor(Math.random()*50),Math.floor(Math.random()*height));
         particlesRight[i] =  new VerletParticle2D(Math.floor(Math.random()*width),Math.floor(Math.random()*height));
         physicsRight.addParticle(particlesRight[i]);
       }
 
       col = getPixel(particlesRight[i].x,particlesRight[i].y);
       fill(col[0],col[1],col[2],125);
-      //rect(particles[i].x-5, particles[i].y-5,10,10);
-      ellipse(particlesRight[i].x, particlesRight[i].y, 7,7);
+      ellipse(particlesRight[i].x, particlesRight[i].y, particleSize,particleSize);
     }
 
     for(let i=0; i<particlesBottom.length;i++){
       if(particlesBottom[i].y <= 1){
         physicsBottom.removeParticle(particlesBottom[i]);
-        //particlesBottom[i] =  new VerletParticle2D(Math.floor(Math.random()*width),height - Math.floor(Math.random()*50));
         particlesBottom[i] =  new VerletParticle2D(Math.floor(Math.random()*width),Math.floor(Math.random()*height));
         physicsBottom.addParticle(particlesBottom[i]);
       }
 
       col = getPixel(particlesBottom[i].x,particlesBottom[i].y);
       fill(col[0],col[1],col[2],125);
-      //rect(particles[i].x-5, particles[i].y-5,10,10);
-      ellipse(particlesBottom[i].x, particlesBottom[i].y, 7,7);
+      ellipse(particlesBottom[i].x, particlesBottom[i].y, particleSize,particleSize);
     }
 
   }else{
