4f59eeb0a521d6e632fcfe63e99a3e12cf920e9d
Adam Nizol <32315521+AdamNizol@users.noreply.github.com>
2020-08-29 17:02:24 -0700

    add mouse attraction

--- Butterfly.js
+++ Butterfly.js
@@ -9,7 +9,7 @@ var  VerletPhysics2D = toxi.physics2d.VerletPhysics2D,
      Vec2D = toxi.geom.Vec2D;
 
 let strokeControl;
-let p1, physics, bg, curImage;
+let mousePos, physics, bg, curImage;
 let ready = false;
 let particles = [];
 let particlesLeft = [];
@@ -28,6 +28,8 @@ getImg(butterflyUrl,960,660);
 
 function setup() {
   strokeControl = document.getElementById("strokeEnabledBox");
+  mousePos = new Vec2D(mouseX, mouseY);
+  let mouseAttraction = 0.5;
 
   createCanvas(962, 720);
   frameRate(60);
@@ -36,9 +38,9 @@ function setup() {
 
   bg = loadImage(butterflyUrl);
   physics.addBehavior(new GravityBehavior(new Vec2D(0,0.1)));
-  p1 = new VerletParticle2D(10,10);
-  physics.addParticle(p1);
-  physics.addBehavior(new AttractionBehavior(p1, 350, 0.1));
+  //p1 = new VerletParticle2D(10,10);
+  //physics.addParticle(p1);
+  physics.addBehavior(new AttractionBehavior(mousePos, 350, mouseAttraction));
 
   for(let i =0; i<600; i++){
     particles.push(new VerletParticle2D(Math.floor(Math.random()*width),Math.random()*660));
@@ -48,8 +50,8 @@ function setup() {
   physicsLeft = new VerletPhysics2D();
   physicsLeft.setWorldBounds(new Rect(0, 0, width, height));
   physicsLeft.addBehavior(new GravityBehavior(new Vec2D(0.1,0)));
-  physicsLeft.addParticle(p1);
-  physicsLeft.addBehavior(new AttractionBehavior(p1, 350, 0.1));
+  //physicsLeft.addParticle(p1);
+  physicsLeft.addBehavior(new AttractionBehavior(mousePos, 350, mouseAttraction));
 
   for(let i =0; i<600; i++){
     particlesLeft.push(new VerletParticle2D(Math.floor(Math.random()*width),Math.random()*660));
@@ -59,8 +61,8 @@ function setup() {
   physicsRight = new VerletPhysics2D();
   physicsRight.setWorldBounds(new Rect(0, 0, width, height));
   physicsRight.addBehavior(new GravityBehavior(new Vec2D(-0.1,0)));
-  physicsRight.addParticle(p1);
-  physicsRight.addBehavior(new AttractionBehavior(p1, 350, 0.1));
+  //physicsRight.addParticle(p1);
+  physicsRight.addBehavior(new AttractionBehavior(mousePos, 350, mouseAttraction));
 
   for(let i =0; i<600; i++){
     particlesRight.push(new VerletParticle2D(Math.floor(Math.random()*width),Math.random()*660));
@@ -70,7 +72,8 @@ function setup() {
   physicsBottom = new VerletPhysics2D();
   physicsBottom.setWorldBounds(new Rect(0, 0, width, height));
   physicsBottom.addBehavior(new GravityBehavior(new Vec2D(0,-0.1)));
-  physicsBottom.addParticle(p1);
+  //physicsBottom.addParticle(p1);
+  physicsBottom.addBehavior(new AttractionBehavior(mousePos, 350, mouseAttraction));
 
   for(let i =0; i<600; i++){
     particlesBottom.push(new VerletParticle2D(Math.floor(Math.random()*width),Math.random()*660));
@@ -82,12 +85,13 @@ function setup() {
 
 function draw() {
   if(ready){
+    mousePos.set(mouseX, mouseY);
     physics.update();
     frameCounter++;
     physicsLeft.update();
     physicsRight.update();
     physicsBottom.update();
-    let framesPerImage = 70;
+    let framesPerImage = 100;
     particleSize = maxParticleSize - ((maxParticleSize - minParticleSize)*(frameCounter/framesPerImage));
     if(frameCounter >= framesPerImage){
       frameCounter = 0;
@@ -104,8 +108,10 @@ function draw() {
     //
     // fill(col[0],col[1],col[2],col[3]);
     // ellipse(p1.x, p1.y, 20,20);
+    let col
 
-    stroke('rgba(0,0,0,0.05)');
+
+    stroke('rgba(0,0,0,0.1)');
     if(strokeControl.checked){
       strokeWeight(1)
     }else{
@@ -162,6 +168,13 @@ function draw() {
   }else{
     background(250, 250, 250);
   }
+
+  col = getPixel(mousePos.x,mousePos.y);
+  fill(col[0],col[1],col[2],col[3]);
+  stroke('rgba(0,0,0,0.4)');
+  strokeWeight(2)
+  ellipse(mousePos.x, mousePos.y, 20,20);
+
 }
 function getPixel(x, y) {
   let pos = ((curImage.width * Math.floor(y)) + Math.floor(x)) * 4;
